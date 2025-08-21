30ea3f497c9fd4a2913c26a38b220f885c318c93
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2025-08-21 09:00:46 +0545

    Delete 04-4.4.1-#!/bin directory

--- 04-4.4.1-#!/bin/04-4.4.2-Restart-Failed-Services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Restarts failed systemd services
-for service in $(systemctl --state=failed | awk 'NR>1 {print $2}'); do
-  echo "Restarting $service..."
-  sudo systemctl restart "$service"
-done
