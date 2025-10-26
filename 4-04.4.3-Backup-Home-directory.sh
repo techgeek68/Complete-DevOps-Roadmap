0cc27c33695b5dd687d69a415eba168d5e640005
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2025-10-26 12:31:29 +0545

    Update and rename 4-04.4.3-Backup-Home-directory.sh to 04.4.3-Backup-Home-directory.sh

--- 4-04.4.3-Backup-Home-directory.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Simple backup of the /home directory
-BACKUP_DIR="/opt/backups"
-DATE=$(date +%F)
-mkdir -p $BACKUP_DIR
-tar czf $BACKUP_DIR/home_backup_$DATE.tar.gz /home
-echo "Backup completed: $BACKUP_DIR/home_backup_$DATE.tar.gz"
